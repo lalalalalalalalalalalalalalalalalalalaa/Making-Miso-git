442d608a2ad28c2b3935031b566f0f305e5eb763
lalala <qazse753753@gmail.com>
2022-05-08 13:36:57 +0800

    20220508-1

--- app/src/main/java/com/example/miso/AlarmManager.java
+++ app/src/main/java/com/example/miso/AlarmManager.java
@@ -3,46 +3,31 @@ package com.example.miso;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
-import android.content.ComponentName;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.IBinder;
-import android.os.PowerManager;
-
-import androidx.annotation.Nullable;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainService extends Service {
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-
-        return null;
-    }
+public class AlarmManager extends BroadcastReceiver {
 
     SQLiteDatabase db;
-
-    private PowerManager.WakeLock wakeLock;
     NotificationManager notificationManager;
-
     final int flags = PendingIntent.FLAG_CANCEL_CURRENT;
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        PowerManager powerManager = (PowerManager)
-                getSystemService(Context.POWER_SERVICE);
-        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "myapp:wakelock");
-        wakeLock.acquire();
+    public void onReceive(Context context, Intent intent) {
 
-        db = openOrCreateDatabase("Miso.db", Context.MODE_PRIVATE, null);
+        SharedPreferences preferences = context.getSharedPreferences("times", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        int count = preferences.getInt("Language", 0);
+
+        db = context.openOrCreateDatabase("Miso.db", Context.MODE_PRIVATE, null);
         Cursor cursor = db.rawQuery("SELECT _id, date FROM Date", null);
 
         String gettime = "";
@@ -52,16 +37,16 @@ public class MainService extends Service {
             while (cursor.moveToNext()) {
                 gettime = cursor.getString(1);
                 days = (int)findTime(gettime);
-                }
+            }
         }
 
-        Notification.Builder builder = new Notification.Builder(MainService.this);
-        notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        Intent push =new Intent(MainService.this,MainService.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(MainService.this,0,push,flags);
+        Notification.Builder builder = new Notification.Builder(context);
+        notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent push =new Intent(context,MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context,0,push,flags);
         builder
-                .setContentTitle(getString(R.string.notify_ttitle))
-                .setContentText(getString(R.string.notify_1_content) + days + getString(R.string.notify_2_content))
+                .setContentTitle(context.getString(R.string.notify_ttitle))
+                .setContentText(context.getString(R.string.notify_1_content) + days + context.getString(R.string.notify_2_content))
                 .setContentIntent(contentIntent)
                 .setTicker("新訊息")
                 //.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher_foreground))
@@ -72,50 +57,21 @@ public class MainService extends Service {
                 .setSmallIcon(R.drawable.home)
                 .setFullScreenIntent(contentIntent, true)//將Notification變為懸掛式Notification
                 .setAutoCancel(true);
+
         Notification notify = builder.build();
 
-        //設定開關，0是關閉，1是開啟
-
-        SharedPreferences preferences = getSharedPreferences("notify", Context.MODE_PRIVATE);
-        int openorclose = preferences.getInt("notify", 0);
-        SharedPreferences.Editor editor = preferences.edit();
-
-        if (days == 0) {
+        if(count <= 6) {
+            count += 1;
+            editor.putInt("times", count);
             notificationManager.notify(0, notify);
-            editor.putInt("notify", 1);
-            editor.apply();
-        }else if ( (days <= 180) && (days % 30 == 0) ) {
-            if (openorclose == 1) {
-                notificationManager.notify(0, notify);
-                editor.putInt("notify", 0);
-                editor.apply();
-            }
-        } else if ( days % 180 == 0 ) {
-            if (openorclose == 1) {
+        }else if(count > 6 && count%6 == 0) {
             notificationManager.notify(0, notify);
-            editor.putInt("notify", 0);
-            editor.apply();
-            }
-        }else {
-            notificationManager.cancel(0);
-            editor.putInt("notify", 1);
-            editor.apply();
         }
         cursor.close();
         db.close();
 
     }
 
-    @Nullable
-    @Override
-    public ComponentName startService(Intent service) {
-        return super.startService(service);
-    }
-
-    @Override
-    public boolean stopService(Intent name) {
-        return super.stopService(name);
-    }
 
     private long findTime(String a){
         long temp = 0;
@@ -134,9 +90,6 @@ public class MainService extends Service {
         return temp;
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
 
-    }
+
 }
--- app/src/main/java/com/example/miso/main.java
+++ app/src/main/java/com/example/miso/main.java
@@ -1,11 +1,15 @@
 package com.example.miso;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +17,15 @@ import android.widget.Button;
 import android.widget.TextView;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 
 public class main extends Fragment {
+
+    private static final String TAG = "tag";
 
     public main() {}
 
@@ -24,6 +33,8 @@ public class main extends Fragment {
     View view;
     SQLiteDatabase db;
     TextView countday;
+
+    Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT+8:00")); //取得時間
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -71,8 +82,8 @@ public class main extends Fragment {
                 start.setVisibility(View.VISIBLE);
                 complete.setVisibility(View.INVISIBLE);
                 countday.setText(getString(R.string.before_start));
-                Intent intent = new Intent(getContext(), MainService.class);
-                getActivity().stopService(intent);
+                cancel_alarm(getContext(),cal);
+
             }
         });
         //設定開始，並將當下時間記錄到資料庫，並且開啟伺服器
@@ -89,12 +100,20 @@ public class main extends Fragment {
                 start.setVisibility(View.INVISIBLE);
                 complete.setVisibility(View.VISIBLE);
                 countday.setText(getString(R.string.btn_starting));
-                Intent intent = new Intent(getContext(), MainService.class);
-                getActivity().startService(intent);
+                //下一分鐘0秒時
+
+                cal.add(Calendar.MONTH, 1);    //加一個月
+                cal.set(Calendar.SECOND, 0);    //設定秒數為0
+                add_alarm(getContext(), cal);        //註冊鬧鐘
+                //在MainActivity中 context = this
+                //在service中      context = context(service的)
+
             }
         });
+
         return view;
     }
+
     //計算天數的方法
     private long findTime(String a){
         long temp = 0;
@@ -114,5 +133,40 @@ public class main extends Fragment {
     public void onDetach() {
         super.onDetach();
         db.close();
+    }
+
+    public static void add_alarm(Context context, Calendar cal) {
+        Log.d(TAG, "alarm add time: " + String.valueOf(cal.get(Calendar.MONTH)) + "." + String.valueOf(cal.get(Calendar.DATE)) + " " + String.valueOf(cal.get(Calendar.HOUR_OF_DAY)) + ":" + cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND));
+
+        Intent intent = new Intent(context, AlarmManager.class);
+        // 以日期字串組出不同的 category 以添加多個鬧鐘
+        intent.addCategory("ID." + String.valueOf(cal.get(Calendar.MONTH)) + "." + String.valueOf(cal.get(Calendar.DATE)) + "-" + String.valueOf((cal.get(Calendar.HOUR_OF_DAY) )) + "." + String.valueOf(cal.get(Calendar.MINUTE)) + "." + String.valueOf(cal.get(Calendar.SECOND)));
+        String AlarmTimeTag = "Alarmtime " + String.valueOf(cal.get(Calendar.HOUR_OF_DAY)) + ":" + String.valueOf(cal.get(Calendar.MINUTE)) + ":" + String.valueOf(cal.get(Calendar.SECOND));
+
+        intent.putExtra("title", "activity_app");
+        intent.putExtra("time", AlarmTimeTag);
+
+        PendingIntent pi = PendingIntent.getBroadcast(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        android.app.AlarmManager am = (android.app.AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        am.set(android.app.AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pi);       //註冊鬧鐘
+    }
+
+    /***    取消(與系統註冊的)鬧鐘    ***/
+    private static void cancel_alarm(Context context, Calendar cal) {
+        Log.d(TAG, "alarm cancel time: " + String.valueOf(cal.get(Calendar.MONTH)) + "." + String.valueOf(cal.get(Calendar.DATE)) + " " + String.valueOf(cal.get(Calendar.HOUR_OF_DAY)) + ":" + cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND));
+
+        Intent intent = new Intent(context, AlarmManager.class);
+        // 以日期字串組出不同的 category 以添加多個鬧鐘
+        intent.addCategory("ID." + String.valueOf(cal.get(Calendar.MONTH)) + "." + String.valueOf(cal.get(Calendar.DATE)) + "-" + String.valueOf((cal.get(Calendar.HOUR_OF_DAY) )) + "." + String.valueOf(cal.get(Calendar.MINUTE)) + "." + String.valueOf(cal.get(Calendar.SECOND)));
+        String AlarmTimeTag = "Alarmtime " + String.valueOf(cal.get(Calendar.HOUR_OF_DAY)) + ":" + String.valueOf(cal.get(Calendar.MINUTE)) + ":" + String.valueOf(cal.get(Calendar.SECOND));
+
+        intent.putExtra("title", "activity_app");
+        intent.putExtra("time", AlarmTimeTag);
+
+        PendingIntent pi = PendingIntent.getBroadcast(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        android.app.AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        am.cancel(pi);    //取消鬧鐘，只差在這裡
     }
 }
